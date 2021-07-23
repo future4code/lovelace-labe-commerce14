4e67b8f33ded2be0c218cbbc68516599ab89318d
Gustavo Guimaraes <Gustavo.carvg@gmail.com>
2021-07-23 14:27:42 -0300

    Inserido o filtro por ordem crescente e decrescente

--- src/components/Filtros.js
+++ src/components/Filtros.js
@@ -1,53 +1,43 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const ContainerFiltros = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 12px;
+
+  form div {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    margin: 12px;
-
-    form div {
-        display: flex; 
-        flex-direction: column;
-        margin: 10px;
-
-    }
-`
+    margin: 10px;
+  }
+`;
 export default class Filtros extends React.Component {
+  render() {
+    return (
+      <ContainerFiltros>
+        <div>
+          <h3>Filtros</h3>
+        </div>
 
-    render() {
-        return (
+        <form>
+          <div>
+            <label>Valor Mínimo</label>
+            <input type="number" placeholder={"Digite o min"} />
+          </div>
 
-            <ContainerFiltros>
+          <div>
+            <label>Valor Máximo</label>
+            <input placeholder={"Digite o max"} />
+          </div>
 
-                <div>
-                <h3>Filtros</h3>
-                </div>    
-
-                <form>
-                    <div>
-                    <label>Valor Mínimo</label>
-                    <input type="number" placeholder={"Digite o min"} />
-                    </div>
-
-                    <div>
-                    <label>Valor Máximo</label>
-                    <input placeholder={"Digite o max"} />
-                    </div>
-
-                    <div>
-                    <label>Busca por nome</label>
-                    <input placeholder={"Nome do produto"} />
-                    </div>
-
-                </form>
-             
-            </ContainerFiltros>
-            
-
-        )
-    }    
-
+          <div>
+            <label>Busca por nome</label>
+            <input placeholder={"Nome do produto"} />
+          </div>
+        </form>
+      </ContainerFiltros>
+    );
+  }
 }
-
