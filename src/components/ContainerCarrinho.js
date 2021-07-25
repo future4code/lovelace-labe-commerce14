88eac1378f42d4142dab606babd664d7fb56f8cf
Gustavo Guimaraes <Gustavo.carvg@gmail.com>
2021-07-25 16:46:29 -0300

    Finalizado os filtros e iniciado o adicionar no carrinho

--- src/components/ContainerCarrinho.js
+++ src/components/ContainerCarrinho.js
@@ -7,12 +7,19 @@ const ContainerCarrinho = styled.div`
 
 export default class Carrinho extends Component {
   render() {
+    const carrinho = this.props.carrinho.map((produto) => {
+      return (
+        <div>
+          <span>{produto.name}</span>
+          <span>{produto.quantity}</span>
+        </div>
+      );
+    });
     return (
       <ContainerCarrinho>
         <h3>Carrinho</h3>
         <div>
-          <span>{this.props.carrinho.name}</span>
-          <span>{this.props.carrinho.quantity}</span>
+          {carrinho}
           <button>Remover</button>
         </div>
         <p>Valor total</p>
